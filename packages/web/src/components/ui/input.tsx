a4a4ecafdd83c781920b2c110f4b34643fd0974f
Davi Giroux <davigiroux@gmail.com>
2026-03-24 07:40:34 -0400

    remove forwardRef (React 19), delete unused card/badge components

--- packages/web/src/components/ui/input.tsx
+++ packages/web/src/components/ui/input.tsx
@@ -1,10 +1,11 @@
-import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  ref?: React.Ref<HTMLInputElement>;
+}
+
+export function Input({ className, type, ref, ...props }: InputProps) {
   return (
     <input
       type={type}
@@ -16,7 +17,4 @@ const Input = React.forwardRef<
       {...props}
     />
   );
-});
-Input.displayName = "Input";
-
-export { Input };
+}
--- packages/web/src/components/ui/label.tsx
+++ packages/web/src/components/ui/label.tsx
@@ -1,19 +1,19 @@
-import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Label = React.forwardRef<
-  HTMLLabelElement,
-  React.LabelHTMLAttributes<HTMLLabelElement>
->(({ className, ...props }, ref) => (
-  <label
-    ref={ref}
-    className={cn(
-      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-      className
-    )}
-    {...props}
-  />
-));
-Label.displayName = "Label";
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  ref?: React.Ref<HTMLLabelElement>;
+}
 
-export { Label };
+export function Label({ className, ref, ...props }: LabelProps) {
+  return (
+    <label
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className
+      )}
+      {...props}
+    />
+  );
+}
